56fd73a9a554c05cd404178fb863b89088d05aff
LieutLet <thesmartalec155@gmail.com>
2025-09-20 22:57:38 -0400

    added base routes that are need for the MVP. They don't return anything other than 'Hello World' and some status messages.

--- Project/api/app.js
+++ Project/api/app.js
@@ -1,7 +1,41 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
+//**************************************************************************************//
+// Get requests                                                                         //
+//**************************************************************************************//
+
+// Route for listing all of the tickets
+app.get('/tickets', function (req, res) {
   res.send('Hello World');
+  app.status(200).json({ message: 'Succesfully retrived all tickets' });
 });
 
+//Retrive single ticket by ticket ID
+app.get('/tickets/:id', function (req, res) {
+  res.send('Hello World');
+  app.status(200).json({ message: 'Ticket response' });
+  app.status(404).json({ message: 'Ticket not found' });
+});
+
+//**************************************************************************************//
+// Post requests                                                                        //
+//**************************************************************************************//
+
+// Route for creating a new ticket
+app.post('/tickets', function (req, res) {
+  res.send('Hello World');
+  app.status(201).json({ message: 'Id of hte ticket you created' });
+  app.status(404).json({ message: 'Invalid Input' });
+});
+
+//**************************************************************************************//
+// delete requests                                                                      //
+//**************************************************************************************//
+
+// Route for archiving a ticket by ticket ID
+app.delete('/tickets/:id', function (req, res) {
+  res.send('Hello World');
+  app.status(200).json({ message: 'Ticket successfully archived' });
+  app.status(404).json({ message: 'Ticket not found' });
+});
