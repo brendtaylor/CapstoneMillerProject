83718a2c267119df755cf2c0564702301163ecd2
kkamendat <165951008+kkamendat@users.noreply.github.com>
2025-10-12 15:52:03 -0400

    Update App.tsx

--- ProjectTempate/UM Flint Quality Template/UM Flint Quality Template/MiHub_React_Template/src/App.tsx
+++ ProjectTempate/UM Flint Quality Template/UM Flint Quality Template/MiHub_React_Template/src/App.tsx
@@ -48,6 +48,7 @@ const AppContent: React.FC = () => {
               <Route path="/documents" element={<Documents />} />
               <Route path="/purchasing" element={<Purchasing />} />
               <Route path="/schedule" element={<Schedule />} />
+              <Route path='/fileform' element={<FileForm />} /> 
             </Routes>
           </motion.div>
       </div>
