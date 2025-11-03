5e1d2ed3cc94dc27d21c0d8d10b15443d807b89b
calebpor <164921333+calebpor@users.noreply.github.com>
2025-11-03 16:56:48 -0500

    Adding image entity file

---
+++ Project/api/src/entities/image.entity.js
@@ -0,0 +1,49 @@
+const { EntitySchema } = require("typeorm");
+
+module.exports = new EntitySchema({
+    name: "Image",
+    tableName: "MiHub_Quality_Images", 
+    columns: {
+        ticketId: {
+            primary: true,      //Forign key
+            type: "int",      
+            name: "TICKETID",  
+            nullable: false,        
+        },
+        id: {
+            primary: true,
+            type: "int",
+            generated: "increment", // Assumes ID is an auto-incrementing key
+            name: "ID",
+        },
+        imageKey: {
+            type: "nvarchar",
+            name: "ImageKey",
+            length: "255",
+            unique: true, // This is very important for searching
+            nullable: false,
+        },
+        imageData: {
+            type: "varbinary",
+            name: "ImageData",
+            length: "max",
+            nullable: false,
+        },
+        mimeType: {
+            type: "nvarchar",
+            name: "MimeType",
+            length: "100",
+            nullable: false,
+        }
+    },
+    relations: {
+        ticket: {
+            target: "ticket", 
+            type: "many-to-one", // An image belongs to one ticket
+            joinColumn: {
+                name: "TICKETID", // The column in this table
+                referencedColumnName: "TICKETID", // The column in the "Ticket" table
+            },
+        },
+    },
+});
