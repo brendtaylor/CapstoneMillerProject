092c2ae8e27a2dd70fee42b754d62dac741c7f8d
kkamendat <165951008+kkamendat@users.noreply.github.com>
2025-11-26 20:06:44 -0500

    Create AssignUser.tsx

---
+++ ProjectTempate/UM Flint Quality Template/UM Flint Quality Template/MiHub_React_Template/src/components/AssignUser.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import { apiPut, apiGet } from "../lib/api";
+
+interface Props {
+  ticketId: number;
+  currenetAssigned?: string;
+}
+
+export default function AssignUser({ ticketId, currentAssigned }: Props) {
+  const [users, setUsers] = useState<string[]>([]);
+  const [selectedUser, setSelectedUser] = useState<string>(currentAssigned || "");
+
+useEffect(() => {
+  async function fetchUsers() {
+    const data = await apiGet<string[]>("/users");
+    setUsers(data);
+  }
+  fetchUsers();
+}, []);
+
+async function handleAssign() {
+  if (!selectedUser) {
+    alert("Please select a user.");
+    return;
+  }
+
+  await apiPut(`/tickets/${ticketId}/assign`, {
+    assignedTo: selectedUser
+  });
+
+  alert("Ticket assigned successfully!);
+}
+
+return (
+  <div style={{ marginTop: "20px" }}>
+    <h3>Assign Ticket</h3>
+
+    <select
+      value={selecteUser}
+      onChange={(e) => setSelectedUser(e.target.value)}
+  >
+      <option value="">Select user...</option>
+      {users.map((u) => (
+        <option key={u} value={u}>
+            {u}
+        </option>
+    ))}
+  </select>
+
+    <button style={{ marginLeft: "10px" }} onClick={handleAssign}>
+      Assign
+    </button>
+  </div>
+);
+}
