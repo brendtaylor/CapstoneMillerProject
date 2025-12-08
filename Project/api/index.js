9d4a03bcff75f2f418bd7cda317153ac9e1cd08d
osartele <osartele@umich.edu>
2025-12-07 21:12:34 -0500

    Update index.js

--- Project/api/index.js
+++ Project/api/index.js
@@ -1,41 +1,33 @@
 //Description: This file is the bridge between the application and the database. It also provides access to the entity files
 
-// load environment variables
-require('dotenv').config();
+require('dotenv').config(); 
 const express = require("express");
 const cors = require('cors');
 
 const mainApiRouter = require("./src/routes/index.js");
-
 const auditRouter = require("./src/routes/audit.js");
-
 const { AppDataSource } = require("./src/data-source");
-
 
 // Initialize database connection
 AppDataSource.initialize()
     .then(() => {
         console.log("Data Source has been initialized!");
 
-        // Create a new Express application instance
         const app = express();
         const port = process.env.PORT || 3000;
 
-        // Middleware to parse incoming JSON requests
         app.use(express.json());
+        
+        // [FIX] Added 'PATCH' to the allowed methods
         app.use(cors({
             origin: ['http://localhost:5173', 'http://localhost:4001', 'https://hoppscotch.io', 'https://web.hoppscotch.io'],
-            methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+            methods: ['GET','POST','PUT','DELETE','OPTIONS','PATCH'], 
             allowedHeaders: ['Content-Type','Authorization', 'cache-control', 'pragma']
         }));
 
-        // Mount the audit routes under /api
-        app.use("/api", auditRouter);
-
-        // main router
+        app.use("/api/audit", auditRouter);
         app.use('/api', mainApiRouter);
 
-        // Start the Express server
         app.listen(port, () => {
             console.log(`Server is running on http://localhost:${port}`);
         });
