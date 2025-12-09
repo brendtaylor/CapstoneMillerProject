620f8fe339158d75c8089db1b7a8466314f33ddb
brendtaylor <brendtay@proton.me>
2025-12-09 12:52:17 -0500

    changed "Save" to "Create"

--- ProjectTempate/UM Flint Quality Template/UM Flint Quality Template/MiHub_React_Template/src/components/FileForm.tsx
+++ ProjectTempate/UM Flint Quality Template/UM Flint Quality Template/MiHub_React_Template/src/components/FileForm.tsx
@@ -511,7 +511,7 @@ const FileForm: React.FC<FileFormProps> = ({ onClose }) => {
                     Clear Draft
                 </button>
                 <button onClick={confirmAndSave} disabled={isSaving} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 disabled:bg-gray-400">
-                    {isSaving ? 'Saving...' : 'Save'}
+                    {isSaving ? 'Saving...' : 'Create'}
                 </button>
             </div>
 
